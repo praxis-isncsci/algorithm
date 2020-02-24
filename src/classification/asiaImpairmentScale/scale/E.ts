397538af74b4615d722dd98226a05c0383cb7666
dakotaJang <dev@dakotajang.com>
2020-02-24 15:37:39 -0800

    update AIS E

--- src/classification/asiaImpairmentScale/scale/E.ts
+++ src/classification/asiaImpairmentScale/scale/E.ts
@@ -1,8 +1,8 @@
 export const checkASIAImpairmentScaleE = (neurologicalLevelOfInjury: string): 'E' | 'E*' | undefined => {
-  if (neurologicalLevelOfInjury.includes('INT')) {
-    return 'E';
-  } else if (neurologicalLevelOfInjury.includes('INT*')) {
+  if (neurologicalLevelOfInjury.includes('INT*')) {
     return 'E*';
+  } else if (neurologicalLevelOfInjury.includes('INT')) {
+    return 'E';
   } else {
     return;
   }
