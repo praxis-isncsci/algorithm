ebc9350309168a6a2c76d6e9f665b5ab2635897a
dakotaJang <dev@dakotajang.com>
2020-10-02 18:31:07 -0700

    refactor injury complete

--- src/classification/injuryComplete/injuryComplete.ts
+++ src/classification/injuryComplete/injuryComplete.ts
@@ -2,47 +2,30 @@ import { Exam, InjuryComplete } from "../../interfaces";
 import { canBeAbsentSensory } from "../common";
 
 export const determineInjuryComplete = (exam: Exam): InjuryComplete => {
-  const rightLightTouchCanBeAbsent = canBeAbsentSensory(exam.right.lightTouch.S4_5);
-  const rightPinPrickCanBeAbsent = canBeAbsentSensory(exam.right.pinPrick.S4_5);
-  const leftLightTouchCanBeAbsent = canBeAbsentSensory(exam.left.lightTouch.S4_5);
-  const leftPinPrickCanBeAbsent = canBeAbsentSensory(exam.left.pinPrick.S4_5);
+  const allS4_5Values = [
+    exam.right.lightTouch.S4_5,
+    exam.right.pinPrick.S4_5,
+    exam.left.lightTouch.S4_5,
+    exam.left.pinPrick.S4_5
+  ];
+
+  if (exam.voluntaryAnalContraction === 'No' && exam.deepAnalPressure === 'No') {
+    if (allS4_5Values.every(v => v === '0')) {
+      return 'C';
+    } else if (allS4_5Values.every(v => ['0','0*'].includes(v))){
+      return 'C*,I*';
+    } else if (allS4_5Values.every(v => ['0','0**'].includes(v))) {
+      return 'I*';
+    }
+  }
 
   if (
-    exam.voluntaryAnalContraction !== 'Yes' && exam.deepAnalPressure !== 'Yes' &&
-    rightLightTouchCanBeAbsent && rightPinPrickCanBeAbsent &&
-    leftLightTouchCanBeAbsent && leftPinPrickCanBeAbsent
+    exam.voluntaryAnalContraction !== 'Yes' &&
+    exam.deepAnalPressure !== 'Yes' &&
+    allS4_5Values.every(canBeAbsentSensory)
   ) {
-    if (
-      exam.voluntaryAnalContraction === 'No' && exam.deepAnalPressure === 'No'
-    ) {
-      if ([
-        exam.right.lightTouch.S4_5,
-        exam.right.pinPrick.S4_5,
-        exam.left.lightTouch.S4_5,
-        exam.left.pinPrick.S4_5
-      ].every(v => v === '0')) {
-        return 'C';
-      } else if ([
-        exam.right.lightTouch.S4_5,
-        exam.right.pinPrick.S4_5,
-        exam.left.lightTouch.S4_5,
-        exam.left.pinPrick.S4_5
-      ].every(v => ['0','0*'].includes(v))){
-        return 'C*,I*';
-      }
-    }
     return 'C,I';
   } else {
-    // return 'I';
-    if (exam.voluntaryAnalContraction === 'No' && exam.deepAnalPressure === 'No' && [
-      exam.right.lightTouch.S4_5,
-      exam.right.pinPrick.S4_5,
-      exam.left.lightTouch.S4_5,
-      exam.left.pinPrick.S4_5
-    ].every(v => ['0','0**'].includes(v))) {
-      return 'I*';
-    } else {
-      return 'I';
-    }
+    return 'I';
   }
 }
